80dc5c24af4babfa933729305fcaa207d2d3e11e
unknown <DAM126@iesataulfo.local>
2024-04-16 15:47:40 +0200

    Quinta fase del proyecto con las clases Clase y Principal creadas, comentadas y codificadas

--- src/directorioTrabajoTorrensVictor/Principal.java
+++ src/directorioTrabajoTorrensVictor/Principal.java
@@ -4,7 +4,11 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// Esta es la clase principal que crea un objeto de la clase "Clase"
+		        // Crear un objeto de la clase Clase
+		        Clase objetoClase = new Clase();
+		    }
+
 
 	}
 
-}
+
