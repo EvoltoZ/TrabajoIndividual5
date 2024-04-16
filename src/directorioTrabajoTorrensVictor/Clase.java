3ad430753581004ff3d5e231090699ac2429f506
unknown <DAM126@iesataulfo.local>
2024-04-16 15:45:53 +0200

    Cuarta fase del proyecto con la clase Clase creada, comentada y codificada y Principal creada y comentada

--- src/directorioTrabajoTorrensVictor/Clase.java
+++ src/directorioTrabajoTorrensVictor/Clase.java
@@ -3,6 +3,12 @@ package directorioTrabajoTorrensVictor;
 public class Clase {
 
 	// Esta es la clase "Clase" que es la plantilla para crear objetos de la clase "Clase"
+	
+	    // Constructor por defecto
+	    public Clase() {
+	        System.out.println("Soy un objeto de la clase y acabo de ser creado");
+	    }
+	}
 
 	
-}
+
